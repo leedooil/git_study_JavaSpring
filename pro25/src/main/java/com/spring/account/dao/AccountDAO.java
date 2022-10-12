df1e033971d54ceb659c1b95ea41c16a4d61e45e
leedooil <107297272+leedooil@users.noreply.github.com>
2022-10-12 11:12:57 +0900

    20221012

---
+++ pro25/src/main/java/com/spring/account/dao/AccountDAO.java
@@ -0,0 +1,8 @@
+package com.spring.account.dao;
+
+import org.springframework.dao.DataAccessException;
+
+public interface AccountDAO {
+	public void updateBalance1 () throws DataAccessException;
+	public void updateBalance2 () throws DataAccessException;
+}
---
+++ pro25/src/main/java/com/spring/account/service/AccountService.java
@@ -0,0 +1,5 @@
+package com.spring.account.service;
+
+public interface AccountService {
+	public void sendMoney() throws Exception;
+}
